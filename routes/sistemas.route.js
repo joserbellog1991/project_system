f9213e0e349df01ad28438f06951918687a0a46a
Jose Bello <joserbellog1991@gmail.com>
2023-03-29 14:29:20 -0300

    añadido sistemas route

--- routes/sistemas.route.js
+++ routes/sistemas.route.js
@@ -6,11 +6,68 @@ router.get('/',(req,res)=>{
 
 res.json({
 
-    message: "propiedades list"
+    message: "sistemas list"
 
 
 });
 
 });
+
+
+router.get('/:i', (req,res)=>{
+   const {id} = req.params; 
+   res.json({
+     message: "id: "+id+" consultado",   
+
+   });
+
+
+});
+
+
+router.post('/',(req,res)=>{
+const body = params.body;
+
+res.json({
+        message:"datos ingresados",
+        data:body
+});
+
+
+});
+
+
+router.patch("/:id",(req,res)=>{
+const {id} = req.params;
+const body = req.body;
+
+if(body){
+
+    res.json({
+            message:"id "+id+" actualizada con exito",
+            data:body
+
+    });
+
+}else{ res.statusCode(404).json({message:"no hay datos para actualizar"});}
+
+});
+
+
+router.delete('/:id',(req,res)=>{
+const {id} = req.params;
+
+if(id){
+    res.json({
+        message:"id "+id+" eliminadacon exito",
+      
+
+});  
+}else {res.statusCode(404).json({message:"no hay datos para actualizar"})}
+    
+
+});
+
+
 
 module.exports = router;
